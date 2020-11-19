61a605e787d6407aae711633fc0d35d8f8b51311
OliverTWang <47228867+OliverTWang@users.noreply.github.com>
2020-11-19 11:18:44 -0500

    Add files via upload

---
+++ emergency.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void emergency(int i){
+  if (i = 1)
+    std::cout << "WARNING: pump capacity is too high." << std::endl;
+  else if (i = 2)
+    std::cout << "WARNING: pump capacity is too low" << std::endl;
+  else if (i = 3)
+    std::cout << "WARNING: intake temperatrue is too high" << std::endl;
+  else if (i = 4)
+    std::cout << "WARNING: intake temperatrue is too low" << std::endl;
+  else if (i = 5)
+    std::cout << "WARNING: outlet temperature is too high" << std::endl;
+  else if (i = 6)
+    std::cout << "WARNING: outlet temperature is too low" << std::endl;
+  else if (i = 7)
+    std::cout << "WARNING: pump pressure is too high" << std::endl;
+  else if (i = 8)
+    std::cout << "WARNING: pump pressure is too low" << std::endl;
+  else if (i = 9)
+    std::cout << "WARNING: reactor power is too high" << std::endl;
+  else if (i = 10)
+    std::cout << "WARNING: reactor power is too low" << std::endl;
+  else
+    std::cout << "EXCEPTION: emergency sequence incorrectly initated" << std::endl;
+}
