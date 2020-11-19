c859c42bcf8122a58d57ec1353206b2bb07b174d
OliverTWang <47228867+OliverTWang@users.noreply.github.com>
2020-11-19 11:08:50 -0500

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 struct package{
   int array1[22];
@@ -8,19 +11,18 @@ struct package{
   double power[10];
 };
 
-double bin1(std::string word); //capacity
-double bin2(std::string word); //feedwater temp
-double bin3(std::string word); //outlet temp
-double bin4(std::string word); //pressure
+int bin1(std::string word); //capacity
+int bin2(std::string word); //feedwater temp
+int bin3(std::string word); //outlet temp
+int bin4(std::string word); //pressure
 
 
-package parser(std::string filename)
-{
+package parser(std::string filename){
     // File pointer
-    fstream fin;
+    std::fstream fin;
 
     // Open the file containing the records
-    fin.open(filename, ios::in);
+    fin.open(filename, std::ios::in);
 
     // Get the roll number
     // of which the data is required
@@ -29,6 +31,12 @@ package parser(std::string filename)
     std::string line, word, temp;
 
     int count = 0;
+
+    int array1[22];
+    int array2[22];
+    int array3[12];
+    int array4[12];
+    double power[10];
 
     while (fin >> temp) {
 
@@ -42,46 +50,54 @@ package parser(std::string filename)
 
         // read every column data of a row and update value of the arrays
         //update static arrays depend on sensor type
-            if (0 <= count << 8) {
+            if (0 <= count < 8) {
               while (getline(s, word, ','))
                 array1[bin1(word)]++;
             }
-            else if(8 <= count << 16) {
+            else if(8 <= count < 16) {
               while (getline(s, word, ','))
                 array2[bin2(word)]++;
             }
-            else if(16 <= count << 24) {
+            else if(16 <= count < 24) {
               while (getline(s, word, ','))
                 array3[bin3(word)]++;
             }
-            else if(24 <= count << 32) {
+            else if(24 <= count < 32) {
               while (getline(s, word, ','))
                 array4[bin4(word)]++;
             }
             else if (count == 32) {
-              int size = 0
+              int size = 0;
               while (getline(s, word, ','))
                 power[size++] = std::stod(word);
             }
+            else
+              break;
         count ++;
     }
+
+
+  package output;
+  for (int i = 0; i < 22; i++) {
+  output.array1[i] = array1[i];
+  output.array2[i] = array2[i];
   }
 
-  package output = {
-    array1[];
-    array2[];
-    array3[];
-    array4[];
-    power[];
+  for (int j = 0; j < 12; j++) {
+  output.array3[j] = array3[j];
+  output.array4[j] = array4[j];
   }
+  for (int k = 0; k < 10; k++)
+  output.power[k] = power[k];
+
 
   return output;
 }
 
-double bin1(std::string word){
+int bin1(std::string word){
   int value;
-  double raw = std::stod(word)
-  if (raw << 6500.0) value = 0;
+  double raw = std::stod(word);
+  if (raw < 6500.0) value = 0;
   else if (raw >= 10000.0) value = 21;
   else {
     value = ((int)raw - 6500)/175 + 1;
@@ -89,10 +105,10 @@ double bin1(std::string word){
   return value;
 }
 
-double bin2(std::string word){
+int bin2(std::string word){
   int value;
-  double raw = std::stod(word)
-  if (raw << 270.0) value = 0;
+  double raw = std::stod(word);
+  if (raw < 270.0) value = 0;
   else if (raw >= 650.0) value = 21;
   else {
     value = ((int)raw - 270)/19 + 1;
@@ -100,10 +116,10 @@ double bin2(std::string word){
   return value;
 }
 
-double bin3(std::string word){
+int bin3(std::string word){
   int value;
-  double raw = std::stod(word)
-  if (raw << 234.0) value = 0;
+  double raw = std::stod(word);
+  if (raw < 234.0) value = 0;
   else if (raw >= 334.0) value = 11;
   else {
     value = ((int)raw - 234)/10 + 1;
@@ -111,10 +127,10 @@ double bin3(std::string word){
   return value;
 }
 
-double bin4(std::string word){
+int bin4(std::string word){
   int value;
-  double raw = std::stod(word)
-  if (raw << 6.4) value = 0;
+  double raw = std::stod(word);
+  if (raw < 6.4) value = 0;
   else if (raw >= 7.4) value = 11;
   else {
     value = (int)((raw - 6.4)/0.1) + 1;
